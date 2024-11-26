7bc71c2385b1e89229e64f9552811f29137dac77
Suchir Nigam <suchirnigam@gmail.com>
2024-11-26 22:15:07 +0530

    lint_erroe

---
+++ src/lint.py
@@ -0,0 +1,28 @@
+import os  # Unused import
+
+def some_function():  # Missing docstring
+    x = 5
+    if x > 3:
+        y = "Hello"
+        print(y)
+    return x
+
+def MyFunction():  # Invalid function name (Pylint prefers snake_case)
+    pass
+
+class MyClass:  # Missing docstring
+    def __init__(self):
+        self.val = 10
+
+    def add(self, a, b):
+        return a + b
+
+def some_long_function_name_that_is_too_complex_to_read():
+    for i in range(100):
+        if i % 2 == 0:
+            print(i)
+        else:
+            for j in range(50):
+                if j % 5 == 0:
+                    print(i * j)
+    return i
